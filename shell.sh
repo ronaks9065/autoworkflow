c7accd7043bbe1928d42c479e32a5dabd7581889
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2024-11-18 16:58:05 +0530

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Mirror clone the source repository
+git clone --mirror https://github.com/sovity/edc-ce.git
+cd edc-ce.git
+
+# Fetch the latest tag
+latest_tag=$(git tag --sort=version:refname | tail -n 1)
+echo "Latest tag: $latest_tag"
+
+# Push the latest tag to the destination repository
+git fetch origin tag "$latest_tag"
+git push https://github.com/ronaks9065/mirror-edc-ce-repo.git main "refs/tags/$latest_tag"
+
+# Clone the destination repository
+cd ..
+git clone https://github.com/ronaks9065/mirror-edc-ce-repo.git
+cd mirror-edc-ce-repo
